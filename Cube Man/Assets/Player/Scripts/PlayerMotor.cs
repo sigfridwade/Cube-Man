a2f7874dd5c1f4b817d2a97d1bee093a45e4a630
sigfridwade <33732358+sigfridwade@users.noreply.github.com>
2017-12-18 09:04:40 +0100

    ...

---
+++ Cube Man/Assets/Player/Scripts/PlayerMotor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Update is called once per frame
+	void Update () {
+		
+		rb.AddForce(0, 0, 200);
+
+	}
+
+}
