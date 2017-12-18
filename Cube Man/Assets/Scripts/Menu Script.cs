a7cc115b7476cab2e68ef13831a4e1d76e8a8f09
sigfridwade <33732358+sigfridwade@users.noreply.github.com>
2017-12-18 15:02:39 +0100

    Area730 added

---
+++ Cube Man/Assets/Scripts/Menu Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+	
+	// Buttons //
+	public void OnPlayClick() {
+
+		Debug.Log("Play Button has ben clicked!");
+
+	}
+
+	public void OnShopClick() {
+
+		Debug.Log("Shop Button has ben clicked!");
+
+	}
+
+}
